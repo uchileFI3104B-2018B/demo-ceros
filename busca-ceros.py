c2fd902b8542e9fe1d4234d579ddf1805d9d0675
thevalentino <valentino.g.gonzalez@gmail.com>
2018-09-24 12:57:31 -0300

    grafica seno y coseno

--- busca-ceros.py
+++ busca-ceros.py
@@ -2,3 +2,19 @@
 Resuelve la ecuacion sin(x) = cos(x) entre 0 y pi.
 Utiliza el metodo de la biseccion.
 """
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+x_to_plot = np.linspace(0, np.pi, 10)
+
+
+plt.clf()
+plt.plot(x_to_plot, np.sin(x_to_plot), label="seno(x)")
+plt.plot(x_to_plot, np.cos(x_to_plot), label="cos(x)")
+
+plt.xlabel("x [radianes]")
+plt.legend()
+
+plt.show()
