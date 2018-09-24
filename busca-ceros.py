e152af3ee94798d4bae716b8862ab63a76e67d50
thevalentino <valentino.g.gonzalez@gmail.com>
2018-09-24 13:24:42 -0300

    implementa el metod de la biseccion
    Ademas grafica cero

--- busca-ceros.py
+++ busca-ceros.py
@@ -7,7 +7,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-x_to_plot = np.linspace(0, np.pi, 10)
+x_to_plot = np.linspace(0, np.pi, 100)
 
 
 plt.clf()
@@ -15,6 +15,34 @@ plt.plot(x_to_plot, np.sin(x_to_plot), label="seno(x)")
 plt.plot(x_to_plot, np.cos(x_to_plot), label="cos(x)")
 
 plt.xlabel("x [radianes]")
-plt.legend()
 
+
+
+def seno_menos_coseno(x):
+    """x en radianes.
+    """
+    output = np.sin(x) - np.cos(x)
+    return output
+
+def biseccion(func, a, b, tol=1e-4):
+    """
+    Busca un cero usando el metodo de la biseccion
+    ...
+    """
+    p = (a + b) / 2 
+    while np.fabs(func(p)) > tol:
+        p = (a + b) / 2 
+        if func(a) * func(p) < 0:
+            b = p
+        elif func(a) * func(p) > 0:
+            a = p
+        else:
+            return p
+    return p
+
+cero_biseccion = biseccion(seno_menos_coseno, 0, 2, tol=1e-7)
+
+plt.axvline(cero_biseccion, color='r', label='cero_biseccion')
+plt.legend()
+plt.savefig('cero_biseccion.png')
 plt.show()
